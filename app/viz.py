56418afd94855bbfab99586958a9254ed9fcb507
omar <omar@Macintosh.local>
2021-01-27 20:18:40 -0800

    updated the visualizations to include push and get requests

--- app/viz.py
+++ app/viz.py
@@ -18,7 +18,7 @@ import pandas as pd
 
 router = APIRouter()
 
-@router.post('/Visualizations/{guest_info}')
+@router.post('/Visualizations')
 async def show_viz(guest_info: int):
    # load the 
    get_feats = predicter(guest_info)
